ac6213c65e6fa5e545c37c7997e3e27a547dac87
Tobias <tobias.lindqvist92@gmail.com>
2022-11-19 00:30:14 +0100

    Changed from Switch statment to If Else, switch was not the thing to do

--- gameJS.js
+++ gameJS.js
@@ -24,38 +24,93 @@ function getPlayerChoice(){
         } 
 }
 // Trying to make a Switch case use for the playRound() function
-function playRoundSwitch(playerSelection, computerSelection){
-     switch (true) {
-            case (playerSelection === computerSelection):
-                console.log(`Computer: ${computerSelection} Player: ${playerSelection}, It is a draw!`)
-                break;
-            case (playerSelection):
-                console.log('case2')
-                break;
-    }
-}
+// How do I translate this function so it works with the game() function and integration wiht it's life implementation
+// Actually does not work for this perticular usecase
+// SCRAPPED FUNCTION
+// function playRoundSwitch(playerSelection, computerSelection){
+//     let win = false
+//      switch (true) {
+//             case (playerSelection === computerSelection):
+//                 win = 'draw';
+//                 return (`Computer: ${computerSelection} Player: ${playerSelection}, It is a draw!`);
+//                 break;
+//             case (playerSelection === "rock" && computerSelection === "paper"):
+//                 win = false;
+//                 return (`Computer wins with ${computerSelection} over ${playerSelection}!`);
+//                 break;
+//             case (playerSelection === "rock" && computerSelection === "scissor"):
+//                 win = true;
+//                 return (`Player wins with ${playerSelection} over ${computerSelection}!`);
+//                 break;
+//             case (playerSelection === "paper" && computerSelection === "scissor"):
+//                 win = false;
+//                 return (`Computer wins with ${computerSelection} over ${playerSelection}!`);
+//                 break;
+//             case (playerSelection === "paper" && computerSelection === "rock"):
+//                 win = true;
+//                 return (`Player wins with ${playerSelection} over ${computerSelection}!`);
+//                 break;
+//             case (playerSelection === "scissor" && computerSelection === "rock"):
+//                 win = false;
+//                 return (`Computer wins with ${computerSelection} over ${playerSelection}!`);
+//                 break;
+//             case (playerSelection === "scissor" && computerSelection === "paper"):
+//                 win = true
+//                 return (`Player wins with ${computerSelection} over ${playerSelection}!`);
+//                 break;
+//             default:
+//                 return (`something went wrong!`)
+//     }
+// }
+
 // Perfect place to use a switch statment for checking results? Might be better!
 function playRound(playerSelection, computerSelection) {
+    let win = false
     if (playerSelection === computerSelection){
-        console.log(`Computer: ${computerSelection} Player: ${playerSelection}, It is a draw!`)
-    } else if (playerSelection < computerSelection){
-        console.log(`Player wins with ${playerSelection} over ${computerSelection}!`)
+        return (`Computer: ${computerSelection} Player: ${playerSelection}, It is a draw!`)
+        //Player Win Scenarios
+    } else if (playerSelection === "rock" && computerSelection === "scissor"){
+        win = true
+        return (`Player wins with ${playerSelection} over ${computerSelection}!`)
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        win = true
+        return (`Player wins with ${playerSelection} over ${computerSelection}!`)
+    } else if (playerSelection === "scissor" && computerSelection === "paper") { 
+        win = true
+        return (`Player wins with ${playerSelection} over ${computerSelection}!`)
+    } else if (playerSelection === "scissor" && computerSelection === "rock") {
+        win = false;
+        return (`Computer wins with ${computerSelection} over ${playerSelection}!`);
+    } else if (playerSelection === "paper" && computerSelection === "scissor") {
+        win = false;
+        return (`Computer wins with ${computerSelection} over ${playerSelection}!`);
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        win = false;
+        return (`Computer wins with ${computerSelection} over ${playerSelection}!`);
     } else {
-        console.log(`Computer wins with ${computerSelection} over ${playerSelection}!`)
+        return ('Something went wrong')
     }
 }
 
 function game() {
+    playRoundSwitch();
+    if (win === "draw"){
+        console.log('Draw nothing happens')
+    } else if (win === true){
+        console.log('VICTORY')
+    } else {
+        console.log('DEFEAT')
+    }
+        
+    
     let l = 5
     for (let l = 5; l > 5; i--);
-    playRound();
         if (i === 0) {
             console.log('Game Over!')
     }
-    console.log("Game Over!")
 }
 
 const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
-// game()
+console.log(playRoundSwitch(playerSelection, computerSelection))
+game()
