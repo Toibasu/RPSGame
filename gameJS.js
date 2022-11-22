9136d82bfe91a09233700749e70075a85db4a964
Tobias <tobias.lindqvist92@gmail.com>
2022-11-22 10:48:39 +0100

    Remade logic for playRound again to make code more readable and neat Need to add score counter and win conditions

--- gameJS.js
+++ gameJS.js
@@ -2,33 +2,69 @@
 function game(){
 let computerScore = 0;
 let playerScore = 0;
-let roundWinner = "";
+let roundWinner = " ";
 
     function playGame() {
         playRound(getPlayerChoice, getComputerChoice);
 
     }
-    function playRound(getPlayerChoice, getComputerChoice) {
-        if (getPlayerChoice === "ROCK" && getComputerChoice === "ROCK") {
-            return "Chose same, tie game";
-        } else if (getPlayerChoice === "ROCK" && getComputerChoice === "SCISSOR") {
-            return "Player win!";
-        } else if (getPlayerChoice === "PAPER" && getComputerChoice === "ROCK") {
-            return "Player win!";
-        } else if (getPlayerChoice === "SCISSOR" && getComputerChoice === "PAPER") {
-            return "Player win!";
-        } else if (getPlayerChoice === "ROCK" && getComputerChoice === "PAPER") {
-            return "Computer win!";
-        } else if (getPlayerChoice === "PAPER" && getComputerChoice === "SCISSOR") {
-            return "Computer win!";
-        } else if (getPlayerChoice === "SCISSOR" && getComputerChoice === "ROCK") {
-            return "Computer win!";
-        } else {
-            return "Something went wrong";
+
+    function playRound_fix(getPlayerChoice, getComputerChoice){
+        if (getPlayerChoice == getComputerChoice){
+            console.log("Tie")
+        } 
+        else if (getPlayerChoice == 'ROCK'){
+            if (getComputerChoice == 'SCISSOR'){
+                console.log("Player Win!")
+                playerScore++;
+            } else {
+                console.log("Computer Win!")
+                computerScore++;
+            }
         }
+        else if (getPlayerChoice == 'PAPER'){
+            if (getComputerChoice == 'ROCK'){
+                console.log("Player win!")
+                playerScore++;
+            }
+            else {
+                console.log("Computer win!")
+                computerScore++;
+            }
+        }
+        else if (getPlayerChoice == 'SCISSOR') {
+            if (getComputerChoice == 'PAPER'){
+                console.log("Player Win!")
+                playerScore++;
+            } else {
+                console.log("Computer win!")
+                computerScore++;
+            }
+
+        }
+
+    // function playRound(getPlayerChoice, getComputerChoice) {
+    //     if (getPlayerChoice === "ROCK" && getComputerChoice === "ROCK") {
+    //         return "Chose same, tie game";
+    //     } else if (getPlayerChoice === "ROCK" && getComputerChoice === "SCISSOR") {
+    //         return "Player win!";
+    //     } else if (getPlayerChoice === "PAPER" && getComputerChoice === "ROCK") {
+    //         return "Player win!";
+    //     } else if (getPlayerChoice === "SCISSOR" && getComputerChoice === "PAPER") {
+    //         return "Player win!";
+    //     } else if (getPlayerChoice === "ROCK" && getComputerChoice === "PAPER") {
+    //         return "Computer win!";
+    //     } else if (getPlayerChoice === "PAPER" && getComputerChoice === "SCISSOR") {
+    //         return "Computer win!";
+    //     } else if (getPlayerChoice === "SCISSOR" && getComputerChoice === "ROCK") {
+    //         return "Computer win!";
+    //     } else {
+    //         return "Something went wrong";
+    //     }
+    // }
     function getPlayerChoice() {
         let playerChoice = prompt("choose")
-
+        
     }
 
     function getComputerChoice() {
@@ -44,14 +80,16 @@ let roundWinner = "";
     }
 
 
-    function gameScore() {
-        
+    function gameScore(playRound) {
+        if (playRound = "Player win!") {
+            playerScore++;
+        }else {
+            computerScore++;
+        }
     }
 
-    function gameOver (playerOptions, movesLeft) {
+    function gameOver(playerOptions, movesLeft) {
         return playerScore === 5 || computerScore === 5
     }
 }
-
-
 getComputerChoice()
